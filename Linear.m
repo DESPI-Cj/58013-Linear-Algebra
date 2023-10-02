6462ec18f0b746f5007bdad65b6b4bb52074a576
DESPI-Cj <123426631+DESPI-Cj@users.noreply.github.com>
2023-10-02 08:28:06 +0800

    Add files via upload

---
+++ Linear.m
@@ -0,0 +1,20 @@
+A = [1, 1, 1; 1, 2, 2; 1, 2, 3];
+
+disp('Determinant of matrix A:');
+a = det(A);
+disp(a);
+
+A1 = [5, 1, 1; 6, 2, 2; 8, 2, 3];
+disp('Determinant of matrix A1:');
+a1 = det(A1);
+disp(a1);
+
+A2 = [1, 5, 1; 1, 6, 2; 1, 8, 3];
+disp('Determinant of matrix A2:');
+a2 = det(A2);
+disp(a2);
+
+A3 = [1, 1, 5; 1, 2, 6; 1, 2, 8];
+disp('Determinant of matrix A3:');
+a3 = det(A3);
+disp(a3);
