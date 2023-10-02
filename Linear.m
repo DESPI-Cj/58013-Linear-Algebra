10a72b1f89b205ab20e65f90860c6160889fa029
DESPI-Cj <123426631+DESPI-Cj@users.noreply.github.com>
2023-10-02 09:28:27 +0800

    Add files via upload

--- Linear.m
+++ Linear.m
@@ -1,5 +1,4 @@
 A = [1, 1, 1; 1, 2, 2; 1, 2, 3];
-
 disp('Determinant of matrix A:');
 a = det(A);
 disp(a);
@@ -18,3 +17,16 @@ A3 = [1, 1, 5; 1, 2, 6; 1, 2, 8];
 disp('Determinant of matrix A3:');
 a3 = det(A3);
 disp(a3);
+
+%==========================================================================
+syms x y z;
+
+eq1 = x + y + z == 5;
+eq2 = x + 2*y + 2*z == 6;
+eq3 = x + 2*y + 3*z == 8;
+
+sol = solve([eq1, eq2, eq3], [x, y, z]);
+xsol = sol.x
+ysol = sol.y
+zsol = sol.z
+
